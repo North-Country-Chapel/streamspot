30a7d24b7f7ced0a238e7690c8afa2a3249297ee
deleriouseel <73902566+deleriouseel@users.noreply.github.com>
2021-09-22 14:56:55 -0700

    added function, fixed logic

--- a_streamspot_getviewers.py
+++ a_streamspot_getviewers.py
@@ -7,23 +7,46 @@ url = 'https://mystreamspot.com'
 values = {'username': '<lol>',
           'password': '<no>'}
 id_num = ""
-dashboard = "https://mystreamspot.com/dashboard"
 mydate = date.today()
+
+def open_session():
+    global session
+    global response
+    global session
+    global url
+    dashboard = "https://mystreamspot.com/dashboard"
+    #open session
+    session = requests.session()
+
+    # log in
+    response = session.post(url + '/login', data=values, allow_redirects=True)
+    
+
+    #test for actual login
+    if str(response.url) != dashboard:
+        time.sleep(30)
+        response = session.post(url + '/login', data=values, allow_redirects=True) 
+    else:
+        #go to analytics page
+        response = session.get(url + '/analytics')
+        
+    return response
 
 
 def get_first_file_id():    
     global response
     global id_num
-      
+
+   
     id_num = str(re.search(r"=([A-Z])\w+==", response.text))
     id_num = id_num[46:59]    
-    print(id_num)
+    
     #get the id_num of the latest study  
     if not id_num:
         time.sleep(30)
         id_num = str(re.search(r"=([A-Z])\w+==", response.text))
         id_num = id_num[46:59]
-
+ 
     return response, id_num
 
 
@@ -33,8 +56,8 @@ def download_file():
     global id_num
 
     #switch to file download page
-    url = (url + '/analytics/export-event-uniques?id' + id_num)
-    response = session.get(url, allow_redirects=True)
+    link = (url + '/analytics/export-event-uniques?id' + id_num)
+    response = session.get(link, allow_redirects=True)
 
 
     #get filename from header
@@ -47,39 +70,31 @@ def download_file():
         file.write(response.content)
         file.close()
 
-#open session
-session = requests.session()
+    return response
 
-# log in
-response = session.post(url + '/login', data=values, allow_redirects=True)
-
-#test for actual login
-if str(response.url) != dashboard:
-    time.sleep(30)
-    response = session.post(url + '/login', data=values, allow_redirects=True)
-    
-else:
-    #go to analytics page
-    response = session.get(url + '/analytics')
-
-
-get_first_file_id()
-download_file()
 
 #get the second newest study for Sunday 1st service
 if mydate.strftime('%w') == '1': 
-
+    open_session()
+    #get the id_num_num of the latest study 
     id_num = re.finditer(r"=([A-Z])\w+==", response.text)
     array = []
     for match in id_num:
         array.append(match.group())
-    id_num = array[1]   
-  
+    id_num = array[1]  
+
+     
     if not id_num:
         time.sleep(30)
         id_num = re.finditer(r"=([A-Z])\w+==", response.text)
         for match in id_num:
             array.append(match.group())
-        id_num = array[1]
+        id_num = array[1]  
 
     download_file()
+
+
+
+open_session()
+get_first_file_id()
+download_file()
