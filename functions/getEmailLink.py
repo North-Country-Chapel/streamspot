80db891d070ec762677387159428cd10ebf58694
deleriouseel <kristin@northcountrychapel.com>
2023-06-28 11:04:18 -0700

    more sleep

---
+++ functions/getEmailLink.py
@@ -0,0 +1,44 @@
+"""
+Automates checking email account for verification email for streamspot scripts.
+Outlook Desktop application MUST be open for the email verification to work. 
+
+"""
+
+import win32com.client
+import re
+import logging
+import time
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename="streamspot.log",
+)
+
+
+def verifyEmail():
+    time.sleep(60)
+    logging.info("Verifying email")
+    outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+
+    inbox = outlook.Folders("Audio.Visual").Folders("Inbox")
+    messages = inbox.Items.Restrict("[Subject] = 'MyStreamSpot - Verify Email'")
+
+    messages.Sort("[ReceivedTime]", False)
+    message = messages.GetLast()
+    logging.info("Message time: " + str(message.ReceivedTime))
+    link_regex = re.compile(
+        "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+    )
+    link = re.findall(link_regex, message.body)[1]
+    logging.info(link)
+
+    message.Unread = False
+    logging.info("Message marked as read")
+
+    return link
+
+
+# https://learn.microsoft.com/en-us/office/vba/api/outlook.items
