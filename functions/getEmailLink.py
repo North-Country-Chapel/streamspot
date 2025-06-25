f3d419a1ffa60f618d6133e009b95aefef292393
deleriouseel <kristin@northcountrychapel.com>
2025-06-25 15:25:22 -0700

    comment out old verify

--- functions/getEmailLink.py
+++ functions/getEmailLink.py
@@ -6,7 +6,6 @@ verifyEmailGraph does not require Outlook to be open.
 """
 
 import requests
-import win32com.client
 import re
 import logging
 import time
@@ -24,27 +23,27 @@ logging.basicConfig(
 )
 
 # Doesn't work with modern (2023) Outlook 
-def verifyEmail():
-    time.sleep(60)
-    logging.info("Verifying email")
-    outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
+# def verifyEmail():
+#     time.sleep(60)
+#     logging.info("Verifying email")
+#     outlook = win32com.client.Dispatch("Outlook.Application").GetNamespace("MAPI")
 
-    inbox = outlook.Folders("Audio.Visual").Folders("Inbox")
-    messages = inbox.Items.Restrict("[Subject] = 'MyStreamSpot - Verify Email'")
+#     inbox = outlook.Folders("Audio.Visual").Folders("Inbox")
+#     messages = inbox.Items.Restrict("[Subject] = 'MyStreamSpot - Verify Email'")
 
-    messages.Sort("[ReceivedTime]", False)
-    message = messages.GetLast()
-    logging.info("Message time: " + str(message.ReceivedTime))
-    link_regex = re.compile(
-        "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-    )
-    link = re.findall(link_regex, message.body)[1]
-    logging.info(link)
+#     messages.Sort("[ReceivedTime]", False)
+#     message = messages.GetLast()
+#     logging.info("Message time: " + str(message.ReceivedTime))
+#     link_regex = re.compile(
+#         "http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+"
+#     )
+#     link = re.findall(link_regex, message.body)[1]
+#     logging.info(link)
 
-    message.Unread = False
-    logging.info("Message marked as read")
+#     message.Unread = False
+#     logging.info("Message marked as read")
 
-    return link
+#     return link
 
 
 # https://learn.microsoft.com/en-us/office/vba/api/outlook.items
